0c4bf7e0a8cd335c46079480f2e754c220e9105d
FRSCO <frscollo@hotmail.it>
2017-07-10 00:46:10 +0100

    initial commit

---
+++ reusable_blog/blog/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+import views
+
+urlpatterns = [
+	url(r'^$', views.post_list),
+	url(r'^(?P<id>\d+)/$', views.post_details),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+	url(r'^(?P<id>\d+)/edit$', views.edit_post, name='edit'),
+
+]
+
---
+++ reusable_blog/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\frsco\hello_django\model_test\blog_prj\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
